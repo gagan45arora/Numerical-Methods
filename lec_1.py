8e079bd9aca0fda1b7f5948be9b1b25f5b74891e
gagan45arora <58810523+gagan45arora@users.noreply.github.com>
2022-01-24 18:06:05 +0530

    Update lec_1.py

--- lec_1.py
+++ lec_1.py
@@ -1,19 +1,5 @@
 data_set=[(0,1),(1,2),(2,4)]
-n=len(data_set)
-def L0(x):
-	val=1
-	val=(x-data_set[1][0])/(data_set[0][0]-data_set[1][0])*(x-data_set[2][0])/(data_set[0][0]-data_set[2][0])
-	return val
-
-def L1(x):
-	val=1
-	val=(x-data_set[2][0])/(data_set[1][0]-data_set[2][0])*(x-data_set[0][0])/(data_set[1][0]-data_set[0][0])
-	return val
-
-def L2(x):
-	val=1
-	val=(x-data_set[0][0])/(data_set[2][0]-data_set[0][0])*(x-data_set[1][0])/(data_set[2][0]-data_set[1][0])
-	return val	
+n=len(data_set)	
 
 def P(x):
 	res=data_set[0][1]*(x-data_set[1][0])/(data_set[0][0]-data_set[1][0])*(x-data_set[2][0])/(data_set[0][0]-data_set[2][0])
